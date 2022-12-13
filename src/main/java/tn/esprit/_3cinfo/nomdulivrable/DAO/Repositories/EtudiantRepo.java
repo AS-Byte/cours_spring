d18ccfc9ec1e9d82584fbf2acda6796a1d1a6f57
AS~byte <amine.oueslati@esprit.tn>
2022-12-13 19:01:35 +0100

    amma ba3d

--- src/main/java/tn/esprit/_3cinfo/nomdulivrable/DAO/Repositories/EtudiantRepo.java
+++ src/main/java/tn/esprit/_3cinfo/nomdulivrable/DAO/Repositories/EtudiantRepo.java
@@ -16,8 +16,10 @@ public interface EtudiantRepo extends CrudRepository<Etudiant,Long> {
     //la méthode qui suit est une application de l'utilisation de JPQL au lieu des keywords, en JPQL on utilise les entités et les attributs, alors que ne SQL on utilise les noms des colonnes et des tables dans la base
     @Query("selECT alias from Etudiant alias where alias.nomE= : lak and alias.prenomE= : esmo")
     Etudiant jibliLEtudiantElliEsmouWLakabouEnParamJPQL(@Param( value = "lak") String lakabu,
-                                                        @Param(value = "esmo") String esmou);//l'ordre n'est pas important
+                                                        @Param(value = "esmo") String esmou);//l'ordre n'est pas important avec la @param
 
-    @Query(value = "SeLeCt * from etudiant where nome=?1 and prenome=?2 ", nativeQuery = true) //l'ordre est très important
+    //la méthode qui suit est application de l'utilisation de SQL au lieu de JPQL
+
+    @Query(value = "SeLeCt * from etudiant where nome=?1 and prenome=?2 ", nativeQuery = true) //l'ordre est très important si pas de @param
     Etudiant jibliLEtudiantElliEsmouWLakabouEnParamSQL(String name, String surname);
 }
